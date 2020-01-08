a3cfd1077ef6bd313cd704e19ea3c5c6839ba6f7
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-08 18:46:18 +0100

    Adds links and logging

--- components/dagpenger.js
+++ components/dagpenger.js
@@ -1,9 +1,13 @@
+import { amplitudeLogger } from '../lib/amplitude-utils'
 import calculateDay from '../lib/calculate-day'
 import prettyPrintDate from '../lib/pretty-print-date'
 
 const DagpengeLink = () => {
+  const handleClick = event => {
+    amplitudeLogger('hurtigveileder.click', { goto: 'dagpengesøknad' })
+  }
   return (
-    <li>Link til dagpengesøknad</li>
+    <li><a href="https://www.nav.no/soknader/nb/person/arbeid/dagpenger" className="underline" target='_blank' rel='noopener noreferrer' onClick={handleClick}>Gå til dagpengesøknad</a></li>
   )
 }
 
@@ -11,6 +15,11 @@ const Dagpenger = props => {
   const { lastDay } = props
   const firstDay = calculateDay(lastDay, 6)
   const now = new Date()
+  const handleClick = event => {
+    const goto = event.target.dataset.goto
+    amplitudeLogger('hurtigveileder.click', { goto })
+    return true
+  }
   return (
     <div className='rounded border px-4 py-4 mb-4'>
       <h2 className='text-2xl'>Søk om Dagpenger</h2>
@@ -19,8 +28,8 @@ const Dagpenger = props => {
       </p>
       <ul>
         {firstDay <= now ? <DagpengeLink /> : null}
-        <li>Link til dagpengekalkulator</li>
-        <li>Link til dagpengesiden</li>
+        <li><a href="https://www.nav.no/arbeid/dagpenger/kalkulator" className="underline" data-goto="dagpengekalkulatoren" target='_blank' rel='noopener noreferrer' onClick={handleClick}>Gå til dagpengekalkulatoren</a></li>
+        <li><a href="https://www.nav.no/no/person/arbeid/dagpenger-ved-arbeidsloshet-og-permittering/dagpenger-nar-du-er-arbeidsledig" className="underline" data-goto="dagpengesiden" target='_blank' rel='noopener noreferrer' onClick={handleClick}>Gå til dagpengesiden</a></li>
       </ul>
     </div>
   )
