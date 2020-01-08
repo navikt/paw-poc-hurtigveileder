e769f5f571003a8449f42c3d0597e52f6de85853
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-08 11:55:58 +0100

    Adds emojis

--- components/tilbakemelding.js
+++ components/tilbakemelding.js
@@ -1,9 +1,17 @@
 const Tilbakemelding = props => {
+  const handleClick = event => {
+    console.log('clicked')
+  }
+
   return (
     <div className="rounded border px-4 py-4 mb-4">
       <p>Hvor nyttig synes du veiledningen var?</p>
-      <div>
-        :-() | :-( | :-| | :-) | :-))
+      <div className="flex">
+        <img src="/images/angry-face.png" alt="Angry emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
+        <img src="/images/sad-face.png" alt="Sad emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
+        <img src="/images/neutral-face.png" alt="Neutral emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
+        <img src="/images/happy-face.png" alt="Smiling emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
+        <img src="/images/mega-happy-face.png" alt="Very happy emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
       </div>
     </div>
   )
