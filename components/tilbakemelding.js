637282275b36d90098cc5f8278d90b79df0d56ca
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-08 08:45:52 +0100

    Updates tilbakemelding

--- components/tilbakemelding.js
+++ components/tilbakemelding.js
@@ -1,7 +1,10 @@
 const Tilbakemelding = props => {
   return (
-    <div>
-      Gi din tilbakemelding
+    <div className="rounded border px-4 py-4 mb-4">
+      <p>Hvor nyttig synes du veiledningen var?</p>
+      <div>
+        :-() | :-( | :-| | :-) | :-))
+      </div>
     </div>
   )
 }
