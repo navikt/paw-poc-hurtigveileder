ca4eac4929985834dc963afe220328281e69bec0
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-08 09:56:16 +0100

    Updates registrering

--- components/registrering.js
+++ components/registrering.js
@@ -1,18 +1,17 @@
 import calculateDay from '../lib/calculate-day'
 
-const Innhold = () => {
-  return (
-    <div>
-      Registrer deg
-    </div>
-  )
-}
-
 const Registrering = props => {
   const { lastDay } = props
   const firstDay = calculateDay(lastDay, 6)
   const now = new Date()
-  return firstDay <= now ? <Innhold /> : null
+  return (
+    <div className="rounded border px-4 py-4 mb-4">
+      <h2 className="text-2xl">Registrer deg</h2>
+      <p>
+        Du bør registrere deg som arbeidssøker <strong>{firstDay <= now ? 'nå' : firstDay.toLocaleDateString()}</strong>
+      </p>
+    </div>
+  )
 }
 
 export default Registrering
