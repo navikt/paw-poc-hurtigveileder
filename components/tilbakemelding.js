5ff7e2b4d2f84ecd6fb6d717b42b566ac785dcd8
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-08 12:44:40 +0100

    Adds height to images

--- components/tilbakemelding.js
+++ components/tilbakemelding.js
@@ -7,11 +7,11 @@ const Tilbakemelding = props => {
     <div className="rounded border px-4 py-4 mb-4">
       <p>Hvor nyttig synes du veiledningen var?</p>
       <div className="flex">
-        <img src="/images/angry-face.png" alt="Angry emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
-        <img src="/images/sad-face.png" alt="Sad emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
-        <img src="/images/neutral-face.png" alt="Neutral emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
-        <img src="/images/happy-face.png" alt="Smiling emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
-        <img src="/images/mega-happy-face.png" alt="Very happy emoji" className="w-16 mr-2 cursor-pointer" onClick={handleClick} />
+        <img src="/images/angry-face.png" alt="Angry emoji" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
+        <img src="/images/sad-face.png" alt="Sad emoji" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
+        <img src="/images/neutral-face.png" alt="Neutral emoji" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
+        <img src="/images/happy-face.png" alt="Smiling emoji" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
+        <img src="/images/mega-happy-face.png" alt="Very happy emoji" className="w-16 h-16 cursor-pointer" onClick={handleClick} />
       </div>
     </div>
   )
