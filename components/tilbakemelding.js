72f3fc4b38f73c08e3925277b4c5e148edde56b1
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-08 13:23:59 +0100

    Updates tilbakemelding

--- components/tilbakemelding.js
+++ components/tilbakemelding.js
@@ -1,17 +1,20 @@
+import { amplitudeLogger } from '../lib/amplitude-utils'
+
 const Tilbakemelding = props => {
   const handleClick = event => {
-    console.log('clicked')
+    const smiley = event.target.dataset.smiley
+    amplitudeLogger('hurtigveileder.tilbakemelding', { smiley })
   }
 
   return (
     <div className="rounded border px-4 py-4 mb-4">
       <p>Hvor nyttig synes du veiledningen var?</p>
       <div className="flex">
-        <img src="/images/angry-face.png" alt="Angry emoji" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
-        <img src="/images/sad-face.png" alt="Sad emoji" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
-        <img src="/images/neutral-face.png" alt="Neutral emoji" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
-        <img src="/images/happy-face.png" alt="Smiling emoji" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
-        <img src="/images/mega-happy-face.png" alt="Very happy emoji" className="w-16 h-16 cursor-pointer" onClick={handleClick} />
+        <img src="/images/angry-face.png" alt="Angry emoji" data-smiley="angry" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
+        <img src="/images/sad-face.png" alt="Sad emoji" data-smiley="sad" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
+        <img src="/images/neutral-face.png" alt="Neutral emoji" data-smiley="neutral" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
+        <img src="/images/happy-face.png" alt="Smiling emoji" data-smiley="smile" className="w-16 h-16 mr-1 cursor-pointer" onClick={handleClick} />
+        <img src="/images/mega-happy-face.png" alt="Very happy emoji" data-smiley="love" className="w-16 h-16 cursor-pointer" onClick={handleClick} />
       </div>
     </div>
   )
--- components/veileder.js
+++ components/veileder.js
@@ -1,8 +1,12 @@
+import dynamic from 'next/dynamic'
 import Arbeidsplassen from './arbeidsplassen'
 import Dagpenger from './dagpenger'
 import Dokumentasjon from './dokumentasjon'
 import Registrering from './registrering'
-import Tilbakemelding from './tilbakemelding'
+const Tilbakemelding = dynamic(
+  () => import('./tilbakemelding'),
+  { ssr: false }
+)
 
 const Veileder = props => {
   const { lastDay } = props
