1bcbdbbfa2eed7c2303d14a9fb16f8ddf9393813
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-08 18:07:01 +0100

    Adds logging of outgoing links

--- components/arbeidsplassen.js
+++ components/arbeidsplassen.js
@@ -1,9 +1,17 @@
+import { amplitudeLogger } from '../lib/amplitude-utils'
+
 const Arbeidsplassen = props => {
+  const handleClick = event => {
+    event.preventDefault()
+    amplitudeLogger('hurtigveileder.click', { goto: 'arbeidsplassen.no' })
+    return true
+  }
+
   return (
     <div className="rounded border px-4 py-4 mb-4">
       <h2 className="text-2xl">Legg inn CV på arbeidsplassen.no</h2>
       <p>
-        På <a href='https://arbeidsplassen.nav.no/' target='_blank' rel='noopener noreferrer'>arbeidsplassen.no</a> finner du mange ledige stillinger.<br />
+        På <a href='https://arbeidsplassen.nav.no/' target='_blank' rel='noopener noreferrer' onClick={handleClick}>arbeidsplassen.no</a> finner du mange ledige stillinger.<br />
         Det er også enkelt å registrere CVen din slik at arbeidsgivere også kan finne deg.
       </p>
     </div>
