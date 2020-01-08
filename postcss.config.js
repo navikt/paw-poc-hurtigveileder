3fc0a5df3b2bf72e8c65cb5b3c56917032894088
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-08 08:19:58 +0100

    Removes unused dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,7 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  whitelistPatterns: [/DayPicker/],
-  content: ['./components/**/*.js', './pages/**/*.js'],
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-})
-
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-  ],
+    require('autoprefixer')
+  ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@ module.exports = {
   theme: {
     fontFamily: {
       display: ['Source Sans Pro', 'sans-serif'],
-      body: ['Source Sans Pro', 'sans-serif'],
+      body: ['Source Sans Pro', 'sans-serif']
     },
     extend: {
       colors: {
-        cyan: '#9cdbff',
+        cyan: '#9cdbff'
       }
     }
   }
