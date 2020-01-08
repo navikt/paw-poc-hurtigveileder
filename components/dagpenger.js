ea27a5ecc76613194b1b8e42fd73cb10fb8aef4e
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-08 08:42:27 +0100

    Updates dagpenger

--- components/dagpenger.js
+++ components/dagpenger.js
@@ -1,12 +1,26 @@
 import calculateDay from '../lib/calculate-day'
 
+const DagpengeLink = () => {
+  return (
+    <li>Link til dagpengesøknad</li>
+  )
+}
+
 const Dagpenger = props => {
   const { lastDay } = props
   const firstDay = calculateDay(lastDay, 6)
   const now = new Date()
   return (
-    <div>
-      Dagpenger: {firstDay <= now ? 'nå' : firstDay.toLocaleDateString()}
+    <div className="rounded border px-4 py-4 mb-4">
+      <h2 className="text-2xl">Søk om Dagpenger</h2>
+      <p>
+        Du bør søke om dagpenger <strong>{firstDay <= now ? 'nå' : firstDay.toLocaleDateString()}</strong>
+      </p>
+      <ul>
+        {firstDay <= now ? <DagpengeLink /> : null}
+        <li>Link til dagpengekalkulator</li>
+        <li>Link til dagpengesiden</li>
+      </ul>
     </div>
   )
 }
