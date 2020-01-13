1e1b60a8089ad82318e436ec1735045010a76d6c
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-13 18:47:32 +0100

    Adds hode/fot (patch)

---
+++ components/nav-fot.js
@@ -0,0 +1,355 @@
+export default () => {
+  return (
+    <footer data-portal-component-type='part' className='sitefooter'>
+      <div className='footernavsection-wrapper'>
+        <div className='footernavsection'>
+          <button className='letter-scroll-left'>
+            <span className='visuallyhidden'>Scroll til begynnelsen av bokstavrekka A-Å</span>
+          </button>
+          <button className='letter-scroll-right'>
+            <span className='visuallyhidden'>Scroll til slutten av bokstavrekka</span>
+          </button>
+          <nav className='container letters-container' id='footer-content-menu' aria-labelledby='footer-nav-heading'>
+            <h1 className='visuallyhidden' id='footer-nav-heading'>Innhold fra A til Å</h1>
+            <ul className='accessible-megafooter'>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=a'
+                >
+                            a
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=b'
+                >
+                            b
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=c'
+                >
+                            c
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=d'
+                >
+                            d
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=e'
+                >
+                            e
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=f'
+                >
+                            f
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=g'
+                >
+                            g
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=h'
+                >
+                            h
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=i'
+                >
+                            i
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=j'
+                >
+                            j
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=k'
+                >
+                            k
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=l'
+                >
+                            l
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=m'
+                >
+                            m
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=n'
+                >
+                            n
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=o'
+                >
+                            o
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=p'
+                >
+                            p
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=q'
+                >
+                            q
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=r'
+                >
+                            r
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=s'
+                >
+                            s
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=t'
+                >
+                            t
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=u'
+                >
+                            u
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=v'
+                >
+                            v
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=w'
+                >
+                            w
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=x'
+                >
+                            x
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=y'
+                >
+                            y
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=z'
+                >
+                            z
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=æ'
+                >
+                            æ
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=ø'
+                >
+                            ø
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+              <li className='letter'>
+                <a
+                  data-ga='Footer: A til AA'
+                  href='/sok/_/service/no.nav.navno/contentAZ?letter=å'
+                >
+                            å
+                </a>
+                <div className='panel-wrapper footer-container'>
+                  <div className='accessible-megafooter-panel' />
+                </div>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div className='container about-site'>
+        <a href='/' data-ga='Footer/Link til NAV' className='decorated'>
+            NAV
+        </a>
+        <ul className='nav' id='insert-vergic-container'>
+          <li><a data-ga='Footer' href='/footer-contactus-no'>Kontakt oss</a></li>
+          <li><a data-ga='Footer' href='/footer-accessibility-no'>Tilgjengelighet</a></li>
+          <li><a data-ga='Footer' href='/personvern'>Personvernerklæring - cookies</a></li>
+          <li><a data-ga='Footer' href='/no/rss'>Nyheter som RSS</a></li>
+        </ul>
+        <div className='copyright'>
+          <p>Arbeids- og velferdsetaten 2020</p>
+        </div>
+      </div>
+    </footer>
+
+  )
+}
