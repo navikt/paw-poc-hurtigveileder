36504178bb4090b9e9cd820f2bd47d312606cc19
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-15 15:57:49 +0100

    Legger inn status i dataset (patch)

--- components/arbeidsplassen.js
+++ components/arbeidsplassen.js
@@ -1,10 +1,16 @@
 import { handleUrlLog } from '../lib/amplitude-utils'
+import daysFromNow from '../lib/days-from-now'
+import calculateStatus from '../lib/calculate-status'
 
 const Arbeidsplassen = props => {
+  const { lastDay } = props
+  const days = daysFromNow(lastDay)
+  const status = calculateStatus(days)
+
   return (
     <div className='bg-white border px-4 py-4 mb-4'>
       <p>
-        <a href='https://arbeidsplassen.nav.no/' className='underline text-blue-600' target='_blank' rel='noopener noreferrer' onClick={handleUrlLog}>Du kan legge inn CVen din og finne ledige stillinger på arbeidsplassen.no</a>
+        <a href='https://arbeidsplassen.nav.no/' data-status={status} className='underline text-blue-600' target='_blank' rel='noopener noreferrer' onClick={handleUrlLog}>Du kan legge inn CVen din og finne ledige stillinger på arbeidsplassen.no</a>
       </p>
     </div>
   )
