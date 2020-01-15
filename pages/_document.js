a11990c6199bf616a36463a047d04293cffc4c36
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-15 07:09:13 +0100

    Moves csp to server config (patch)

--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,9 @@ class MyDocument extends Document {
   }
 
   render () {
-    const csp = 'default-src \'none\'; script-src \'self\'; style-src \'self\' \'unsafe-inline\' https://fonts.googleapis.com; connect-src \'self\' https://amplitude.nav.no; object-src \'none\'; img-src \'self\' data:; font-src \'self\' https://fonts.gstatic.com;'
     return (
       <Html lang='no'>
         <Head>
-          <meta httpEquiv='Content-Security-Policy' content={csp} />
           <link href='images/favicon.ico' rel='icon' type='image/x-icon' />
         </Head>
         <body className='bg-gray-100'>
